b99350e93ff4806f352abce9eec9f6f8c2483a3f
bradserlis <bradserlis@yahoo.com>
2020-04-08 16:57:20 -0700

    init

---
+++ src/anagrams.js
@@ -0,0 +1,10 @@
+import { isEqual } from 'lodash';
+
+import { getLetterCount } from './letter-count';
+
+export const isAnagram = (string1, string2) => {
+    const string1LetterCount = getLetterCount(string1);
+    const string2LetterCount = getLetterCount(string2);
+
+    return isEqual(string1LetterCount, string2LetterCount);
+}
