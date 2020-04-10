ff8e87bb7d768d992168f4c830c737a6fce84fea
bradserlis <bradserlis@yahoo.com>
2020-04-09 21:46:07 -0700

    add test helpers and reset calls for integration test

--- tdd-server/src/db.test.js
+++ tdd-server/src/db.test.js
@@ -1,20 +1,12 @@
 import { expect } from 'chai';
 import { getUserByUsername } from './db';
-import { MongoClient } from 'mongodb';
+import { setDatabaseData, resetDatabase, getDatabaseData } from './test-helpers';
 
 describe('get user by username', () => {
+    afterEach('reset the database', async () => {
+        await resetDatabase();
+    })
     it('get correct user from database given a username', async () => {
-        const client = await MongoClient.connect(
-            `mongodb://localhost:27017/TEST_DB`,
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true
-            },
-        )
-
-        const db = client.db('TEST_DB');
-
-        // tests go here.....
 
         const fakeData =
             [
@@ -30,11 +22,10 @@ describe('get user by username', () => {
                 }
             ]
 
-        await db.collection('users').insertMany(fakeData);
+        await setDatabaseData('users', fakeData);
+
         const actual = await getUserByUsername('abc');
-        const finalDBState = await db.collection('users').find().toArray();
-        await db.dropDatabase();
-        client.close();
+        const finalDBState = await getDatabaseData('users');
 
         const expected = {
             id: '123',
